88373655fe07285d917f7f4cf975dcd9bcd5f255
MOZGIII <mike-n@narod.ru>
2020-02-14 15:33:26 +0300

    Provide outputs with an underscore proper notation and deprecated the old ones

--- global/outputs.tf
+++ global/outputs.tf
@@ -1,8 +1,17 @@
+# DEPRECATED
 output "aws_s3_bucket-vector-tests-arn" {
   value = aws_s3_bucket.vector-tests.arn
 }
 
+# DEPRECATED
 output "aws_s3_bucket-vector-test-athena-results-arn" {
   value = aws_s3_bucket.vector-test-athena-results.arn
 }
 
+output "aws_s3_bucket_vector_tests_arn" {
+  value = aws_s3_bucket.vector-tests.arn
+}
+
+output "aws_s3_bucket_vector_test_athena_results_arn" {
+  value = aws_s3_bucket.vector-test-athena-results.arn
+}
