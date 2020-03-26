3a8d13aaaadb17ae5409ccf45e20d3a40eec12d9
Luke Steensen <luke.steensen@gmail.com>
2020-03-26 11:18:36 -0500

    remove unneeded stuff
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- cases/file_to_s3_reliability/terraform/variables.tf
+++ cases/file_to_s3_reliability/terraform/variables.tf
@@ -18,10 +18,6 @@ variable "user_id" {
   type = string
 }
 
-variable "test_harness_aws_account_ids" {
-  type = list(string)
-}
-
 // don't actually need this, but need to accept it
 variable "results_s3_bucket_name" {
   type = string
