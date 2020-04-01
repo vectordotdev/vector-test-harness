45b96cfb77c6d016e328a6f7935b2699ae7ccd86
MOZGIII <mike-n@narod.ru>
2020-04-02 00:01:07 +0300

    Allow terraform interactivity if requested
    Use this at your own risk! It's counter-intuitive, but local references
    to the remote terraform state are currently shared accross user_id and
    test_configuration, and you should not migrate them! Instead, clear local
    state when switching the configuration or user id.
    It'll be addressed in the future.

--- lib/vector-test-harness/terraform.sh
+++ lib/vector-test-harness/terraform.sh
@@ -5,6 +5,7 @@ prepare_terraform_params() {
   local VERBOSE="${1:-"false"}"
 
   TERRAFORM_INIT_EXTRA_ARGS=()
+  TERRAFORM_COMMON_EXTRA_ARGS=()
 
   if [[ -n "${TF_PLUGIN_DIR:-""}" ]]; then
     TERRAFORM_INIT_EXTRA_ARGS+=(
@@ -14,5 +15,18 @@ prepare_terraform_params() {
     if [[ "$VERBOSE" != "false" ]]; then
       echo "Terraform plugins dir: $TF_PLUGIN_DIR"
     fi
+  fi
+
+  if [[ "${TF_INTERACTIVE:-""}" == "true" ]]; then
+    TERRAFORM_COMMON_EXTRA_ARGS+=(
+      "-input=true"
+    )
+    if [[ "$VERBOSE" != "false" ]]; then
+      echo "Terraform will accept interactive input"
+    fi
+  else
+    TERRAFORM_COMMON_EXTRA_ARGS+=(
+      "-input=false"
+    )
   fi
 }
